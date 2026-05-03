99688ffe4052689027dd2d69a3d2e6eb3b959d49
Wahei's MacBook Air <fujisakiwahei@gmail.com>
2026-05-03 12:55:33 +0900

    @を用いた相対パスに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dig-and-build.com",
   output: "static",
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+  },
   integrations: [
     sitemap({
       filter: (page) => !page.includes("/404"),
